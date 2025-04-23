f33a2ece7561a089d4357c5fc10c9c5dc8136e3b
Growtopia Jaw <growtopiajaw@gmail.com>
2025-04-23 15:21:03 +0800

    Modify server.py - Remove GET request support - Modify POST request logging output - Remove access log output - Modify logging format - Remove Starting & Stopping httpd messages

--- server.py
+++ server.py
@@ -16,31 +16,26 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("\"%s\"", post_data.decode('utf-8'))
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
+    def log_message(self, format, *args):
+        return
+
 def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format="{asctime} - {message}", style="{", datefmt="[%d/%b/%Y %H:%M:%S]")
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    logging.info('Stopping httpd...\n')
 
 if __name__ == '__main__':
     from sys import argv
